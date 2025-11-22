071d7e9102b58faecd79bacc28adea9ffb7ea680
Tony Gash <gigert9@gmail.com>
2025-11-22 15:39:20 -0500

    Begin Historical Data Module Implementation
    - Added Historical Bar Data for past 30 days by Minute
    - Abstracted get_stock_list to separate function for modular implementation
    - Began work on Historical Quote Data

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from datetime import datetime, timedelta
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-from alpaca.data import StockHistoricalDataClient, StockLatestQuoteRequest
+from alpaca.data import StockHistoricalDataClient, StockLatestQuoteRequest, StockBarsRequest, TimeFrame, StockQuotesRequest
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
+MONGODB_URI = os.getenv("MONGODB_URI")
+mongo_client = MongoClient(MONGODB_URI)
 trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
 historical_client = StockHistoricalDataClient(API_KEY, API_SECRET)
 
@@ -22,11 +27,14 @@ def process_action(user_action):
             place_order()
         case "status":
             get_order_status()
+        case "historical":
+            get_historical_data()
         case "help":
             print('Possible actions:')
             print('"Info" = Show account buying power')
             print('"Quote" = Enter stock names to get current price')
             print('"Order" = Issue a Market Order')
+            print('"Historical" = Get historical data for past 30 days')
             print('"Status" = Show status of Open Orders')
             print('"Help" = Show this menu')
             print('"Exit" = Exit program')
@@ -66,17 +74,21 @@ def get_account_info():
     menu()
 
 ###########################################
-def get_stock_info():
+def get_stock_list():
     user_input = True
     stock_list = []
-
     while user_input:
         provided_stock = input("Enter Stocks Ticker Names, when Done enter 'Done': ")
         if provided_stock == "Done":
             user_input = False
         else:
             stock_list.append(provided_stock)
-        
+    
+    return stock_list
+
+###########################################
+def get_stock_info():
+    stock_list = get_stock_list()
     print(stock_list)
 
     # multi symbol request - single symbol is similar
@@ -122,6 +134,57 @@ def place_order():
 def get_order_status():
     orders = trading_client.get_orders()
     print(f'Open Orders: \n{orders}')
+    menu()
+
+###########################################
+def get_historical_data():
+    stock_list = get_stock_list()
+    print(stock_list)
+    current_datetime = datetime.now()
+    thirty_days_ago = current_datetime - timedelta(days=30)
+
+    bar_request_params = StockBarsRequest(
+        symbol_or_symbols=stock_list,
+        timeframe=TimeFrame.Minute,
+        start=thirty_days_ago,
+        end=current_datetime
+    )
+
+    quote_request_params = StockQuotesRequest(
+        symbol_or_symbols=stock_list,
+        start=thirty_days_ago,
+        end=current_datetime,
+        limit=20
+    )
+
+    bars = historical_client.get_stock_bars(bar_request_params)
+    bars.df
+    quotes = historical_client.get_stock_quotes(quote_request_params)
+
+    for stock in stock_list:
+        ask_data = []
+        print(f'## BAR DATA FOR: {stock}')
+        print(bars[stock])
+        print(f'## HISTORICAL QUOTE DATA FOR: {stock}')
+        for q in quotes[stock]:
+            ask_data.append({
+                'timestamp': q.timestamp,
+                'ask_price': q.ask_price
+            })
+        df = pd.DataFrame(ask_data)
+
+        if df.empty:
+            print('No data avaialble for this period.')
+            continue
+
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+        #Resample Historical Quotes by minute
+        df.set_index('timestamp', inplace=True)
+        minute_summary = df['ask_price'].resample('1min').mean()
+        for ts, ask in minute_summary.dropna().items():
+            print(f'{ts}: {ask}')
+    
     menu()
 
 ###########################################
